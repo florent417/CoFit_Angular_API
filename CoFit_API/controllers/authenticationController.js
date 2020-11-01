2efe965d98f0bc8b94c228f6a59ead66920ffcbe
Berthelmaster <berthel0802@gmail.com>
2020-11-01 16:18:55 +0100

    added bodyparser

--- CoFit_API/controllers/authenticationController.js
+++ CoFit_API/controllers/authenticationController.js
@@ -2,5 +2,9 @@ var express = require('express');
 
 // Login User
 module.exports.login = function (req, res) {
+    res.status(200).send(req.body);
+}
+
+module.exports.register = function (req,res) {
     res.status(200).send("hello");
 }
--- CoFit_API/routes/index.js
+++ CoFit_API/routes/index.js
@@ -1,12 +1,13 @@
 var express = require('express');
 var router = express.Router();
-var ctrllogin = require('../controllers/loginController');
+var ctrlauth = require('../controllers/authenticationController');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.get('/login', ctrllogin.login)
+router.post('/login', ctrlauth.login)
+router.post('/register', ctrlauth.register)
 
 module.exports = router;
