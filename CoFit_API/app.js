9a9e451ee8f9d96d3293d36a5a13fde73a155624
Florent Avdijaj <flole@live.dk>
2020-11-01 15:55:31 +0100

    Added db conn

--- CoFit_API/app.js
+++ CoFit_API/app.js
@@ -2,9 +2,31 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const mongoose = require("mongoose");
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+
+
+let dbURI = "mongodb://localhost/CoFitDb";
+if (process.env.NODE_ENV === 'production') {
+  dbURI = process.env.MONGODB_URI;
+}
+
+/*
+// HEROKU
+mongoose.connect(dbURI, { 
+  useNewUrlParser: true, 
+  useUnifiedTopology: true,
+  useFindAndModify: false 
+});
+*/
+
+// TODO: Remove, test purposes
+mongoose.connection.on("connected", () => {
+    console.log(`DB started with string: ${dbURI}`);
+    console.log(mongoose.connection.name);
+});
 
 var app = express();
 
